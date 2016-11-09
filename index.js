cb7ff6c7edb874b72f8b3d8a43304784d9405c87
Andrew Lowe <doodlefolks@gmail.com>
2016-11-09 15:37:41 -0800

    Add files route

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 let express = require('express');
+let fs = require('fs');
 let app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+app.get('/files', (req, res) => {
+  let root = fs.readdir('/');
+  res.send(root);
+})
 
 app.listen(80);

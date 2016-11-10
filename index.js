ae83f3bb9b7796a9acc6e3d96600b663fdd8d6de
Andrew Lowe <doodlefolks@gmail.com>
2016-11-10 11:34:08 -0800

    add some promises

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
 let express = require('express');
 let fs = require('fs');
+let readdir = require('fs-readdir-promise');
 let app = express();
 let os = require('os');
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
-app.get('/files', (req, res) => {
-  let root = fs.readdir('/', (err, files) => {
-    res.send(files);
-  });
-})
-
-app.get('/os', (req, res) => {
-  fs.readFile('/proc/cpuinfo', 'utf8', (err, data) => {
-    res.send(data);
-  });
+app.get('/*', (req, res) => {
+  let dir = req.url;
+  dir = dir.replace(/\+*/, ' ');
+  console.log(dir);
+  let data = [];
+  let promises = [];
+  readdir(dir)
+    .then((files) => {
+      for (let file of files) {
+        let newPromise = readdir(`${dir}/${file}`)
+          .then((files) => {
+            data.push({
+              name: file,
+              type: 'dir',
+              path: `${dir}${file}`,
+            })
+          })
+          .catch((err) => {
+            data.push({
+              name: file,
+              type: 'file',
+              path: `${dir}/${file}`,
+            })
+          })
+        ;
+        promises.push(newPromise);
+      }
+      Promise.all(promises).then(() => {
+        res.send(data);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.end();
+    })
+  ;
 });
 
 app.listen(80);

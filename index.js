2abe0cb8a75a03cceae9a5737ccab90a680e231b
Andrew Lowe <doodlefolks@gmail.com>
2016-11-10 09:45:45 -0800

    Add cpus

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let fs = require('fs');
 let app = express();
+let os = require('os');
 
 app.get('/', (req, res) => {
   res.send('Hello World');
@@ -11,5 +12,10 @@ app.get('/files', (req, res) => {
     res.send(files);
   });
 })
+
+app.get('/os', (req, res) => {
+  let cpus = os.cpus();
+  res.send(cpus);
+});
 
 app.listen(80);
